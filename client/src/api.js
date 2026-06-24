95a170cdea57699c04b2b62e46c1302e0a47db63
dddim-wang <dddimwang@gmail.com>
2026-06-24 14:27:49 +0800

    Fix production socket origin and use jpg background

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000'
+const defaultApiBase =
+  typeof window === 'undefined' ? 'http://localhost:5000' : window.location.origin
+
+export const API_BASE = import.meta.env.VITE_API_BASE || defaultApiBase
 
 export const api = axios.create({ baseURL: API_BASE })
 
